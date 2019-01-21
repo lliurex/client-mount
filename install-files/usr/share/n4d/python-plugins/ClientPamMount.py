1f8dc7224be2a5a09f7a44fc217145a57354b782
hectorgh <hectorgh@gmail.com>
2019-01-21 09:28:42 +0100

    adding source files

---
+++ install-files/usr/share/n4d/python-plugins/ClientPamMount.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+import os
+import shutil
+import tempfile
+import threading
+
+class ClientPamMount:
+	
+	def __init__(self):
+		
+		self.pam_skel="/etc/security/pam_mount.conf.xml.lliurex.skel"
+		self.pam_file="/etc/security/pam_mount.conf.xml.lliurex"
+		self.key="%%SERVER%%"
+		
+	#def init
+
+
+	def startup(self,options):
+		
+		if options["boot"]:
+			t = threading.Thread(target=self._startup)
+			t.daemon = True
+			t.start()
+		
+	#def startup
+
+
+	def _startup(self):
+		
+		try:
+			# revert to server first thing, instead of waiting for a 10times timeout
+			self.set_address("server")
+			# Then try to get server ip
+			self.configure_xml()
+			
+		except Exception as e:
+			print(str(e))
+			
+	#def _startup
+	
+	
+	def uchmod(self,file,mode):
+		
+		prevmask = os.umask(0)
+		os.chmod(file,mode)
+		os.umask(prevmask)
+		
+	#def uchmod
+
+	def set_address(self,address):
+		
+		if os.path.exists(self.pam_skel):
+			
+			f=open(self.pam_skel)
+			tmp,filename=tempfile.mkstemp()
+			tmp_file = open(filename,'w')
+			for line in f.readlines():
+				tmp_file.write(line.replace(self.key,address))
+				
+			f.close()
+			tmp_file.close()
+			self.uchmod(filename,0644)
+				
+			shutil.copy(filename,self.pam_file)
+			os.remove(filename)
+			
+			return True
+			
+		return False
+		
+	#def set_address
+
+	
+	def configure_xml(self):
+
+		configured=False
+
+		# Making sure we're able to read SRV_IP var from server
+		tries=10
+		for x in range(0,tries):
+			
+			ip=objects["VariablesManager"].get_variable("SRV_IP")
+			if ip != None:
+				
+				if self.set_address(ip):
+					configured=True
+				break
+				
+			else:
+				# lets sleep and try again
+				time.sleep(1)
+		
+		if configured:
+			return {"status":True,"msg":"Configured to %s"%ip}
+		else:
+			# fallback to 'server'
+			return {"status":False,"msg":"Fallen back to 'server'"}
+
+	#def configure_xml
+	
